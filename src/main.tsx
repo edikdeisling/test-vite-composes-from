ca7dade89917cf18393a473fd3c6585fa1411b7c
Deisling Eduard <eduard.deisling@soveren.io>
2021-04-22 13:03:14 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styles from './index.module.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className={styles.container} />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
